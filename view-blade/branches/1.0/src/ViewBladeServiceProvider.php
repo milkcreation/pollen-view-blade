8af0d52271bf082e295aedc37d82d9873ade2fc6
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-06-24 08:25:06 +0000

    view-blade >> 1.0.x-dev

---
+++ view-blade/branches/1.0/src/ViewBladeServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\ViewBlade;
+
+use Pollen\Container\BootableServiceProvider;
+
+class ViewBladeServiceProvider extends BootableServiceProvider
+{
+
+}
